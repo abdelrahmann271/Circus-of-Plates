19b0ef06c95d438d6687bc88bd3ae5c92ee5a304
Hagrass <Hagrass@DESKTOP-RTRINL0>
2019-12-13 16:47:18 +0200

    k

--- src/Worlds/GameWorld.java
+++ src/Worlds/GameWorld.java
@@ -1,3 +1,4 @@
+<<<<<<< Upstream, based on origin/ayman
 package Worlds;
 
 import java.io.IOException;
@@ -129,3 +130,96 @@ public class GameWorld implements World {
 	}
 
 }
+=======
+package Worlds;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import Objects.*;
+import Objects.Plate;
+import eg.edu.alexu.csd.oop.game.GameObject;
+import eg.edu.alexu.csd.oop.game.World;
+
+public class GameWorld implements World {
+	
+	//All the Game logic will be Here !
+	
+	private final int width ;
+	private final int height;	
+	private final List<GameObject> constant = new LinkedList<GameObject>();
+	private final List<GameObject> moving = new LinkedList<GameObject>();
+	private final List<GameObject> control = new LinkedList<GameObject>();
+	
+	public GameWorld(int screenWidth, int screenHeight) throws IOException {
+		
+		this.width = screenWidth;
+		this.height = screenHeight;
+		
+		PotPlate pp = new PotPlate(100,100);
+		pp.SetSpriteImages();
+		control.add(pp);
+		
+		Bar bar1 = new Bar();
+		
+		
+		
+	}
+
+	@Override
+	public List<GameObject> getConstantObjects() {
+
+		return constant;
+	}
+
+	@Override
+	public List<GameObject> getMovableObjects() {
+
+		return moving;
+	}
+
+	@Override
+	public List<GameObject> getControlableObjects() {
+
+		return control;
+	}
+
+	@Override
+	public int getWidth() {
+
+		return this.width;
+	}
+
+	@Override
+	public int getHeight() {
+
+		return this.height;
+	}
+
+	@Override
+	public boolean refresh() {
+
+		return false;
+	}
+
+	@Override
+	public String getStatus() {
+
+		return null;
+	}
+
+	@Override
+	public int getSpeed() {
+
+		return 0;
+	}
+
+	@Override
+	public int getControlSpeed() {
+
+		return 0;
+	}
+
+}
+>>>>>>> c563127 k
