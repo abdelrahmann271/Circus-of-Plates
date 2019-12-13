ef40df02ef6ded71b54340c5ab9958568bcd559d
Hagrass <Hagrass@DESKTOP-RTRINL0>
2019-12-13 17:10:35 +0200

    1

--- src/Objects/Plate.java
+++ src/Objects/Plate.java
@@ -2,6 +2,7 @@ package Objects;
 
 import java.awt.Color;
 import java.awt.Graphics;
+<<<<<<< Upstream, based on origin/ayman
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -160,3 +161,172 @@ public abstract class Plate extends JPanel implements GameObject  {
 	}
 
 }
+=======
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+import eg.edu.alexu.csd.oop.game.GameObject;
+
+
+
+public abstract class Plate extends JPanel implements GameObject  {
+	int x=400;
+	int y=500;
+	int height;
+	int dx;
+	BufferedImage[] SpriteImages; 
+	int color;
+	boolean isVisible=true;
+	String type;
+	//X and Y 
+	
+	Plate() throws IOException
+	{
+		
+	}
+	public Plate(int X,int Y) throws IOException
+	{
+		this.x=X;
+		this.y=Y;
+		this.SetSpriteImages();
+	}
+	public void setType(String type)
+	{
+		 this.type=type;
+	}
+	public String getType()
+	{
+		return type;
+	}
+	
+	@Override
+	public int getX() {
+		// TODO Auto-generated method stub
+		return x;
+	}
+
+	@Override
+	public void setX(int x) {
+		this.x=x;
+		
+	}
+
+	@Override
+	public int getY() {
+		// TODO Auto-generated method stub
+		return y;
+	}
+
+	@Override
+	public void setY(int y) {
+		// TODO Auto-generated method stub
+		this.y=y;
+		
+	}
+	public void setHeightFromClown(int h) {
+		this.height=h;
+	}
+	public int getHeightFromClown() {
+		return this.height;
+	}
+    public void setShiftX(int dx) {
+    	this.dx=dx;
+    }
+    public int getShiftX() {
+    	return dx;
+    }
+	@Override
+	public int getWidth() {
+		// TODO Auto-generated method stub
+		int Width=SpriteImages[0].getWidth();
+		return Width;
+	}
+
+	@Override
+	public int getHeight() {
+		// TODO Auto-generated method stub
+		int Height=SpriteImages[0].getHeight();
+		return Height;
+	}
+
+	@Override
+	public boolean isVisible() {
+		// TODO Auto-generated method stub
+		return isVisible;
+	}
+	public void setVisible(boolean Visible)
+	{
+		this.isVisible=Visible;
+	}
+	/**
+	 * 
+	 * @param color
+	 * number 0 is the black one 
+		//number 1 is the blue one 
+		// number 2 is the cyan one
+		// number 3 is the darkred one
+		// number 4 is the gold one
+		// number 5 is the green one
+		// number 6 is the orange one 
+		// number 7 is the pink
+		//number 8 is purple
+		// number 9 is red
+		// number 10 is yellow
+	 */
+	public void setColor(int color)
+	{
+		this.color=color;
+		
+	}
+	public int getColor()
+	{
+		return color;
+	}
+	public BufferedImage getImage()
+	{
+		return SpriteImages[color];
+	}
+
+	@Override
+	public BufferedImage[] getSpriteImages() {
+		// TODO Auto-generated method stub
+		return SpriteImages;
+	}
+	
+	public void draw(Graphics g)
+	{
+		if(isVisible==false)
+		{
+			return;
+		}
+		BufferedImage img=this.getImage();
+		g.setColor(Color.orange);
+		g.fillRect(0, 75+this.getHeight(),400,10);
+		g.drawImage(img, getX(), getY(), this);
+	}
+	public  void SetSpriteImages() throws IOException
+	{
+		BufferedImage[] out=new BufferedImage[11];
+		File file;
+		String path="plates";
+		//System.out.println(type+ " ff");
+		String []color= {"black","blue","cyan","darkred"
+				,"gold","green","orange","pink","purple","red","yellow"};
+		for(int i=0;i<11;i++)
+		{   
+			file=new File(path+"\\"+color[i]+type+".png");
+			out[i]=ImageIO.read(file);
+		}
+		this.SpriteImages=out;
+	
+	}
+	
+
+}
+
+>>>>>>> 3f97cef 1
--- src/View/GUItestFeatures.java
+++ src/View/GUItestFeatures.java
@@ -1,3 +1,4 @@
+<<<<<<< Upstream, based on origin/ayman
 package View;
 
 import java.awt.EventQueue;
@@ -115,3 +116,122 @@ public class GUItestFeatures extends javax.swing.JFrame {
 	}
 }
 
+=======
+package View;
+
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+
+import Worlds.GameWorld;
+import eg.edu.alexu.csd.oop.game.GameEngine;
+import eg.edu.alexu.csd.oop.game.GameEngine.GameController;
+
+import java.awt.Color;
+
+
+
+public class GUItestFeatures extends javax.swing.JFrame {
+	
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JFrame frame;
+	//Values in the constructor will change
+	GameWorld gameWorldFirstLevel = new GameWorld(1000,1000); //Just trying the game engine with any world
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUItestFeatures window = new GUItestFeatures();
+					window.frame.setVisible(true);
+					window.frame.setExtendedState(JFrame.MAXIMIZED_BOTH);	//Make The Game window take all the screen				
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});	
+	}
+	/**
+	 * Create the application.
+	 * @throws IOException 
+	 */
+	public GUItestFeatures() throws IOException {
+		getContentPane().setEnabled(false);
+		getContentPane().setLayout(null);
+		initialize();
+	}
+	/**
+	 * Initialize the contents of the frame.
+	 * @throws IOException 
+	 */
+	private void initialize() throws IOException {
+		
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//Just Put the background image path .. 
+		File file = new File("C:\\Users\\Abdelrahman Nour\\Desktop\\Tent2.png");
+		BufferedImage myImage = ImageIO.read(file);
+		frame.setContentPane(new Background(myImage));
+		//Adding the JMenu bar for the game engine controllers...
+		JMenuBar  menuBar = new JMenuBar();
+		JMenu menu = new JMenu("File");
+		menu.setBackground(Color.YELLOW);
+		JMenuItem newMenuItem = new JMenuItem("New");
+		JMenuItem pauseMenuItem = new JMenuItem("Pause");
+		JMenuItem resumeMenuItem = new JMenuItem("Resume");
+		menu.add(newMenuItem);
+		menu.addSeparator();
+		menu.add(pauseMenuItem);
+		menu.add(resumeMenuItem);
+		menuBar.add(menu);
+		frame.setJMenuBar(menuBar);
+		
+		final GameController gameController = GameEngine.start("Very Simple Game in 99 Line of Code", gameWorldFirstLevel, menuBar, Color.BLACK);
+		newMenuItem.addActionListener(new ActionListener() {
+		@Override public void actionPerformed(ActionEvent e) {
+				gameController.changeWorld(gameWorldFirstLevel);
+			}
+		});
+		pauseMenuItem.addActionListener(new ActionListener() {
+		@Override public void actionPerformed(ActionEvent e) {
+				gameController.pause();
+			}
+		});
+		resumeMenuItem.addActionListener(new ActionListener() {
+			@Override public void actionPerformed(ActionEvent e) {
+				gameController.resume();
+			}
+		});
+		
+		
+
+
+	}
+}
+
+>>>>>>> 3f97cef 1
