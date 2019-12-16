b7605d7b8c6c49936a1eaf4bc752c61187550a33
Abdelrahman Hagrass <bodyhagrass@gmail.com>
2019-12-16 12:24:10 +0000

    PotPlate.java edited online with Bitbucket

--- src/Objects/PotPlate.java
+++ src/Objects/PotPlate.java
@@ -11,13 +11,20 @@ public class PotPlate extends Plate  {
 		this.SetSpriteImages();
 		// TODO Auto-generated constructor stub
 	}
+	PotPlate() throws IOException {
+		super();
+		this.setColor(color);
+		setType("pot");
+		this.SetSpriteImages();
+		// TODO Auto-generated constructor stub
+	}
 	public PotPlate(int X,int Y) throws IOException
 	{
 		super();
 		this.x=X;
 		this.y=Y;
 		setType("pot");
-		this.setColor((int) ((Math.random()*11)));
+		//this.setColor((int) ((Math.random()*11)));
 		this.SetSpriteImages();
 	}
 
