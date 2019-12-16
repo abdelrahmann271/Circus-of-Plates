98b2ea7023b76be415c2b58e9a6ff8d20a4542de
Abdelrahman Hagrass <bodyhagrass@gmail.com>
2019-12-16 12:22:08 +0000

    NonBasedPlate.java edited online with Bitbucket

--- src/Objects/NonBasedPlate.java
+++ src/Objects/NonBasedPlate.java
@@ -4,10 +4,20 @@ import java.io.IOException;
 
 public class NonBasedPlate  extends Plate {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	NonBasedPlate(int color) throws IOException {
 		super();
 		setType("platewithoutbase");
 		this.setColor(color);
+		SetSpriteImages();
+		// TODO Auto-generated constructor stub
+	}
+	NonBasedPlate() throws IOException {
+		super();
+		setType("platewithoutbase");
 		SetSpriteImages();
 		// TODO Auto-generated constructor stub
 	}
@@ -16,7 +26,7 @@ public class NonBasedPlate  extends Plate {
 		super();
 		this.x=X;
 		this.y=Y;
-		this.setColor((int) ((Math.random()*11)));
+		//this.setColor((int) ((Math.random()*11)));
 		setType("platewithoutbase");
 		SetSpriteImages();
 	}
