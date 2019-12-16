0568e6d0b3d2583de434cd2e94e2308645722cef
Abdelrahman Hagrass <bodyhagrass@gmail.com>
2019-12-16 12:25:22 +0000

    DeepPlate.java edited online with Bitbucket

--- src/Objects/DeepPlate.java
+++ src/Objects/DeepPlate.java
@@ -12,10 +12,20 @@ import eg.edu.alexu.csd.oop.game.GameObject;
 public class DeepPlate extends Plate {
 	
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	DeepPlate(int color) throws IOException {
 		super();
 		this.type="platewithdeepbase";
 		this.setColor(color);
+		this.SetSpriteImages();
+		// TODO Auto-generated constructor stub
+	}
+	DeepPlate() throws IOException {
+		super();
+		this.type="platewithdeepbase";
 		this.SetSpriteImages();
 		// TODO Auto-generated constructor stub
 	}
@@ -25,7 +35,6 @@ public class DeepPlate extends Plate {
 		this.x=X;
 		this.y=Y;
 		this.type="platewithdeepbase";
-		this.setColor((int) ((Math.random()*11)));
 		this.SetSpriteImages();
 		
 	}
